dc6498fa13e28791743a82cff729a9e1e047d912
vanya <vanya@DESKTOP-776LL6R>
2026-01-22 22:02:03 +0500

    Добавьте файлы проекта.

---
+++ Library.Core.CoffeeHouse/ProductsAndRooms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.CoffeeHouse;
+
+public class ProductsAndRooms
+{
+    public Product? Product { get; set; }
+    public Room? Room { get; set; }
+}
